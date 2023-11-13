33dfb75fc25d04c7d245744bdd2a0005019b9aea
Calvin Das <calvindas7@gmail.com>
2023-11-13 17:38:02 +0530

    error fix

--- Server/routes/User.js
+++ Server/routes/User.js
@@ -1,7 +1,7 @@
 import express from "express";
 const router = express.Router();
 
-import { signin, signup } from "../controllers/user.js";
+import { signin, signup } from "../Controllers/user.js";
 
 router.post("/signin", signin);
 router.post("/signup", signup);
